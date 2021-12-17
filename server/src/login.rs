6764ef081cbee28712c6ed819708a8ccb2cb8991
main() <main@ehvag.de>
2021-12-17 19:52:11 +0100

    update server

--- server/src/login.rs
+++ server/src/login.rs
@@ -10,10 +10,10 @@ pub struct LoggedIn {
 
 const USERID: &str = "userid";
 #[rocket::async_trait]
-impl<'a, 'r> FromRequest<'a, 'r> for LoggedIn {
+impl<'a> FromRequest<'a> for LoggedIn {
     type Error = ();
-    
-    async fn from_request(req: &'a rocket::Request<'r>) -> Outcome<Self, Self::Error> {
+
+    async fn from_request<'r>(req: &'a rocket::Request<'r>) -> Outcome<Self, Self::Error> {
         let jar = <&'a CookieJar>::from_request(req).await.unwrap();
         let steamid = match jar.get_private(USERID).and_then(|x| x.value().parse().ok()) {
             Some(x) => x,
@@ -32,7 +32,7 @@ pub async fn login<'a>(returnurl: String) -> Redirect {
 pub async fn login_cb<'a>(cookies: &'a CookieJar<'a>, qs: &'a Origin<'a>) -> Redirect {
     if let Some(q) = qs.query() {
         // TODO: rework the error handling here
-        let (req, verifier) = Verifier::from_querystring(q).unwrap();
+        let (req, verifier) = Verifier::from_querystring(&*q).unwrap();
         let (parts, body) = req.into_parts();
         let resp = reqwest::Client::new()
             .post(&parts.uri.to_string())
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,7 @@
-use rocket_contrib::json::Json;
-use rocket_contrib::serve::StaticFiles;
-use rocket::response::NamedFile;
 use rocket::State;
 
+use rocket::fs::{NamedFile, FileServer};
+use rocket::serde::json::Json;
 use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
 
 use kutschfahrt::State as KutschfahrtState;
@@ -17,8 +16,8 @@ use error::{Result, Error};
 
 
 #[rocket::get("/me", rank = 1)]
-async fn me_loggedin<'a>(db: State<'a, SqlitePool>, l: LoggedIn) -> Result<Json<MyState>> {
-    let my_games = sqlx::query_scalar!("SELECT gameid FROM game_players WHERE steamid = ?", l.steamid).fetch_all(&*db).await?;
+async fn me_loggedin(db: &State<SqlitePool>, l: LoggedIn) -> Result<Json<MyState>> {
+    let my_games = sqlx::query_scalar!("SELECT gameid FROM game_players WHERE steamid = ?", l.steamid).fetch_all(&**db).await?;
     Ok(Json(MyState::LoggedIn { my_games }))
 }
 #[rocket::get("/me", rank = 2)]
@@ -26,13 +25,13 @@ fn me_loggedout() -> Json<MyState> { Json(MyState::LoggedOut) }
 
 
 #[rocket::get("/game/<id>")]
-async fn game_get<'a>(db: State<'a, SqlitePool>, id: String, l: LoggedIn) -> Result<Json<GameInfo>> {
-    let state = sqlx::query_scalar!("SELECT state FROM game_state WHERE gameid = ?", id).fetch_optional(&*db).await?;
-    let you = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ? AND steamid = ?", id, l.steamid).fetch_optional(&*db).await?;
+async fn game_get(db: &State<SqlitePool>, id: String, l: LoggedIn) -> Result<Json<GameInfo>> {
+    let state = sqlx::query_scalar!("SELECT state FROM game_state WHERE gameid = ?", id).fetch_optional(&**db).await?;
+    let you = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ? AND steamid = ?", id, l.steamid).fetch_optional(&**db).await?;
     let you = you.and_then(|x| x.parse().ok());
     Ok(Json(match (state, you) {
         (None, you) => {
-            let players = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ?", id).fetch_all(&*db).await?;
+            let players = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ?", id).fetch_all(&**db).await?;
             let players = players.into_iter().map(|x| x.parse().unwrap()).collect();
             GameInfo::WaitingForPlayers { players, you }
         }
@@ -45,32 +44,32 @@ async fn game_get<'a>(db: State<'a, SqlitePool>, id: String, l: LoggedIn) -> Res
 }
 
 #[rocket::post("/game/<id>", data = "<cmd>")]
-async fn game_post<'a>(cmd: Json<GameCommand>, db: State<'a, SqlitePool>, id: String, l: LoggedIn) -> Result<()> {
-    let state = sqlx::query_scalar!("SELECT state FROM game_state WHERE gameid = ?", id).fetch_optional(&*db).await?;
+async fn game_post(cmd: Json<GameCommand>, db: &State<SqlitePool>, id: String, l: LoggedIn) -> Result<()> {
+    let state = sqlx::query_scalar!("SELECT state FROM game_state WHERE gameid = ?", id).fetch_optional(&**db).await?;
     match (cmd.into_inner(), state) {
         (GameCommand::JoinGame(player), None) => {
             let player = player.to_string();
-            sqlx::query!("INSERT INTO game_players(gameid, steamid, player_character) VALUES (?, ?, ?)", id, l.steamid, player).execute(&*db).await?;
+            sqlx::query!("INSERT INTO game_players(gameid, steamid, player_character) VALUES (?, ?, ?)", id, l.steamid, player).execute(&**db).await?;
         }
         (GameCommand::LeaveGame, None) => {
-            sqlx::query!("DELETE FROM game_players WHERE gameid = ? AND steamid = ?", id, l.steamid).execute(&*db).await?;
+            sqlx::query!("DELETE FROM game_players WHERE gameid = ? AND steamid = ?", id, l.steamid).execute(&**db).await?;
         }
         (GameCommand::StartGame, None) => {
-            let players = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ?", id).fetch_all(&*db).await?;
+            let players = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ?", id).fetch_all(&**db).await?;
             let players = players.into_iter().map(|x| x.parse().unwrap()).collect();
 
             let state = KutschfahrtState::new(players, &mut rand::thread_rng());
 
             let state = serde_json::to_string(&state)?;
-            sqlx::query!("INSERT INTO game_state(gameid, state) VALUES (?, ?)", id, state).execute(&*db).await?;
+            sqlx::query!("INSERT INTO game_state(gameid, state) VALUES (?, ?)", id, state).execute(&**db).await?;
         }
         (GameCommand::Command(c), Some(s)) => {
-            let you = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ? AND steamid = ?", id, l.steamid).fetch_one(&*db).await?;
+            let you = sqlx::query_scalar!("SELECT player_character FROM game_players WHERE gameid = ? AND steamid = ?", id, l.steamid).fetch_one(&**db).await?;
             let you = you.parse().unwrap();
             let mut state: KutschfahrtState = serde_json::from_str(&s)?;
             state.apply_command(you, c)?;
             let state = serde_json::to_string(&state)?;
-            sqlx::query!("UPDATE game_state SET state = ? WHERE gameid = ?", state, id).execute(&*db).await?;
+            sqlx::query!("UPDATE game_state SET state = ? WHERE gameid = ?", state, id).execute(&**db).await?;
         }
         _ => return Err(Error::CommandDoesNotMatchGameState),
     }
@@ -91,10 +90,10 @@ async fn create_db_pool() -> Result<SqlitePool> {
 }
 
 #[rocket::launch]
-async fn rocket() -> rocket::Rocket {
-    rocket::ignite()
+async fn rocket() -> _ {
+    rocket::build()
         .manage(create_db_pool().await.unwrap())
-        .mount("/", StaticFiles::from("../client/dist"))
+        .mount("/", FileServer::from("../client/dist"))
         .mount("/", rocket::routes![spa_fallback])
         .mount("/api/", rocket::routes![
             login::login,
